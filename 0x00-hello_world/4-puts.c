df86f4f5daa6be5243380beee9c74f5542110ea3
nelaheavean <celiakana@yahoo.com>
2023-02-27 09:39:19 -0800

    4-puts-c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main(void) {
+/**
+* Documetation
+* main - prints a string of characters
+* return (0) if success
+*/
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
