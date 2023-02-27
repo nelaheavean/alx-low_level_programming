dae91fb946ad1c028067f5189876b10de49ce9b1
nelaheavean <celiakana@yahoo.com>
2023-02-27 13:40:08 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints the size of various computer size
- * return 0 (Sucess)
- */
+* main - using sizeof to dynamically determine the size of types char, int and float
+*
+* Return: Always 0.
+*/
 int main(void)
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of float: %lu byte()\n", (unsigned long)sizeof(f));
-return (0);
+{
+ int n;
+ printf("Size of type 'char' on my computer: %lu bytes\n", sizeof(char));
+ printf("Size of type 'int' on my computer: %lu bytes\n", sizeof(int));
+ printf("Size of type 'float' on my computer: %lu bytes\n", sizeof(float));
+ printf("Size of type of my variable n on my computer: %lu bytes\n", sizeof(n));
+ return (0);
 }
