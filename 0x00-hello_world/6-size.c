43e48cfd18aab7496a68e7f6df4a15dd4fd058f7
nelaheavean <celiakana@yahoo.com>
2023-02-27 13:49:07 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,9 +8,10 @@
 int main(void)
 {
  int n;
- printf("Size of type 'char' on my computer: %lu bytes\n", sizeof(char));
- printf("Size of type 'int' on my computer: %lu bytes\n", sizeof(int));
- printf("Size of type 'float' on my computer: %lu bytes\n", sizeof(float));
- printf("Size of type of my variable n on my computer: %lu bytes\n", sizeof(n));
+ printf("Size of a char: %lu bytes\n", sizeof(char));
+ printf("Size of an int: %lu bytes\n", sizeof(int));
+ printf("Size of a long int: %lu bytes\n", sizeof(long int));
+ printf("Size of a long long int: %lu bytes\n", sizeof(long long int));
+ printf("Size of a float: %lu bytes\n", sizeof(float));
  return (0);
 }
