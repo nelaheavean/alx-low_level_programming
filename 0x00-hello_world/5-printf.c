213d52b47267676123aa4afa30a7dcba04faf363
nelaheavean <celiakana@yahoo.com>
2023-02-27 01:29:16 -0800

    5-print-f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,6 @@
-#include <stdio.h>                                                   
-/**                                                            
- * Description: main - Prints out a string to stdout.                         
- * Return: 0 if success. 
-*/                                                                     
-int main(void){                       
+#include <stdio.h>
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
